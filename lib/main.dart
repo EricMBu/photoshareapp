a4182e01d2167c2f2d60dbb73d96c83f88bf3ad1
Eric Buchanan <ericmichael.buchanan@gmail.com>
2021-11-16 19:13:07 -0500

    Added photo display

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // import 'dart:html';
 
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -19,15 +21,23 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final ImagePicker _picker = ImagePicker();
 
-  List<XFile>? _imageFileList;
+  List<Uint8List> _imageFileList = [];
 
-  set _imageFile(XFile? value) {
-    _imageFileList = value == null ? null : [value];
-  }
+  // set _imageFile(XFile? value) {
+  //   _imageFileList = value == null ? null : [value];
+  // }
 
   Future getImage() async {
-    final XFile? photo =
-        await _picker.pickImage(source: ImageSource.gallery, imageQuality: 50);
+    final XFile? photo = await _picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 50,
+    );
+    if (photo != null) {
+      final bytes = await photo.readAsBytes();
+      setState(() {
+        _imageFileList.add(bytes);
+      });
+    }
   }
 
   @override
@@ -37,16 +47,22 @@ class _HomeState extends State<Home> {
         title: const Text("PhotoShare"),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          "add a photo",
-          style: TextStyle(
-            fontSize: 20.0,
-            color: Colors.grey[600],
-            fontFamily: 'Poppins',
-          ),
-        ),
-      ),
+      // body: Center(
+      //   child: Text(
+      //     "add a photo",
+      //     style: TextStyle(
+      //       fontSize: 20.0,
+      //       color: Colors.grey[600],
+      //       fontFamily: 'Poppins',
+      //     ),
+      //   ),
+      // ),
+      body: ListView.builder(
+          itemCount: _imageFileList.length,
+          itemBuilder: (context, index) {
+            final image = _imageFileList[index];
+            return Image.memory(image);
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: getImage,
         child: const Text("add photo"),
